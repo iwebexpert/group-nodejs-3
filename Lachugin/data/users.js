c905277e2bec0ba749fb93d5f16d0da02c38c2d2
Vladimir <VladimirLa4ygin@yandex.ru>
2020-10-27 22:07:45 +0300

    lessons4

---
+++ Lachugin/data/users.js
@@ -0,0 +1,11 @@
+module.exports = {
+  anna: {
+    username: "Anna",
+    age: 25,
+    skills: ["JS", "React.js"],
+  },
+  ivan: {
+    username: "Ivan",
+    skills: ["JS", "Node.js", "React.js"],
+  },
+};
