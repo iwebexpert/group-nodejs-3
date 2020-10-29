ce7fe245627abf6d2491e4f0032d0bc40764d5ce
rusla <suhrugen@gmail.com>
2020-10-29 18:52:17 +0700

    стили

--- sukhorukov/hw5/models/task.js
+++ sukhorukov/hw5/models/task.js
@@ -7,6 +7,10 @@ const taskSchema = new Schema({
         type: String,
         required: true,
     },
+    description: {
+        type: String,
+        default: "Подробности отсутствуют",
+    },
     completed: {
         type: Boolean,
         default: false,
