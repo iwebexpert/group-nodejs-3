ad2885a5b02bb0c652d453a86405d0994cd71826
juliana <julianajarm@gmail.com>
2020-10-31 17:16:08 +0300

    lesson 4 homework finished

--- JulianaJarm/project/express.js
+++ JulianaJarm/project/express.js
@@ -1,12 +1,43 @@
+const DEFAULT_NEWS_COUNT = 5
 const express = require('express')
+const request = require('request')
 const hbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
+const cheerio = require('cheerio')
 const path = require('path')
 const users = require('./data/users')
 
+
 const app = express()
+
+//todo replace work with config with a class
+function extractNewsConfig(req) {
+    let config = req.cookies.config
+    if (config === undefined) {
+        config = "{}"
+    }
+    let {count, fresh_only} = JSON.parse(config)
+    fresh_only = !!fresh_only
+
+    if (count === undefined || count <= 0) {
+        count = DEFAULT_NEWS_COUNT
+    }
+    return {count, fresh_only}
+}
+
+function saveNewsConfig(req, res) {
+    let {count, fresh_only} = req.body
+    fresh_only = fresh_only === 'on'
+    count = +count
+    if (count <= 0) {
+        count = DEFAULT_NEWS_COUNT
+    }
+    res.cookie('config', JSON.stringify({count, fresh_only}))
+}
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 app.use(express.static('public'))
 
 
@@ -20,18 +51,15 @@ app.engine('hbs', hbs({
 app.set('view engine', 'hbs')
 
 
-// работа с заголовками
 app.use((req, res, next) => {
-    if(req.headers.test === '1234'){
+    if (req.headers.test === '1234') {
         req.test = 'user header test sent'
     }
     next()
 })
 
-
 app.get('/', (req, res) => {
-    res.status(200).send('hello')
-
+    res.redirect('/news')
 })
 
 app.post('/users', (req, res) => {
@@ -39,13 +67,8 @@ app.post('/users', (req, res) => {
     res.status(200).send('POST!')
 })
 
-app.post('/users', (req, res) => {
-    console.log(req.body)
-    res.redirect('/config')
-})
-
 app.get('/users', (req, res) => {
-    if(req.test) {
+    if (req.test) {
         console.log(req.test)
     } else {
         console.log('Header TEST was sent')
@@ -59,22 +82,67 @@ app.get('/users/:username', (req, res) => {
 
     const user = users[username]
 
-    if(!user) {
+    if (!user) {
         res.status(404).render('error')
     }
 
     res.render('user', {user})
 })
 
-
 app.get('/config', (req, res) => {
-    res.render('config')
+    const config = extractNewsConfig(req)
+    res.render('config', config)
 })
 
+
+app.post('/config', (req, res) => {
+    saveNewsConfig(req, res)
+    res.redirect('/news')
+})
+
+app.get('/news', (req, res) => {
+    const config = extractNewsConfig(req)
+    request('https://habr.com/ru/feed/', (err, response, body) => {
+        if (!err && response.statusCode === 200) {
+            const $ = cheerio.load(body)
+            const news = []
+            $('div.news-topic', '.news-block__content')
+                .filter((i, el) => {
+                    if (!config.fresh_only) {
+                        return true
+                    }
+                    const time = $(el).find('span.news-topic__attr_date-time').text()
+                    return time.indexOf('вчера') === -1
+                })
+                .slice(0, config.count)
+                .each((i, el) => {
+                    const link = $(el).find('a').first()
+                    news.push({href: link.attr('href'), title: link.text().trim()})
+                })
+            res.render('news', {news})
+        }
+    })
+
+})
+
+app.post('/news', (req, res) => {
+    console.log(req.body)
+})
+
+app.get('/cookie/get', (req, res) => {
+    console.log(req.cookies)
+    res.send(JSON.stringify(req.cookies))
+})
+
+app.get('/cookie/set', (req, res) => {
+    res.cookie('count', Math.floor(Math.random()))
+    res.redirect('/cookie/get')
+})
 
 app.get('*', (req, res) => {
     res.status(404).render('error')
 })
+
 
 app.listen(4000, () => {
     console.log('http://localhost:4000')
