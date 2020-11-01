8261841de47b0e5100d6807a394365efafd5c063
KostaKA2020 <konstakozh@yandex.ru>
2020-11-01 22:24:19 +0400

    lesson6

--- kozhevnikov/lesson61/index.js
+++ kozhevnikov/lesson61/index.js
@@ -3,8 +3,13 @@ const mongoose = require('mongoose')
 const path = require('path')
 const exphbs = require('express-handlebars')
 const taskRoutes = require('./routes/tasks')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const PORT = process.env.PORT || 3000
+
+const userModel = require('./models/user')
+const passport = require('./auth')
 
 const app = express()
 const hbs = exphbs.create({
@@ -18,6 +23,17 @@ app.set('views', 'views')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(session({
+  resave: true,
+  saveUninitialized: false,
+  secret: '1234dfshkhgfoifdguoifdsgufisdugfjug',
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
+app.use(passport.initialize)
+app.use(passport.session)
+
+app.use('/tasks', passport.mustBeAuthenticated)
 
 app.use(taskRoutes)
 
@@ -37,5 +53,33 @@ async function start() {
     console.log(e)
   }
 }
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+  const {repassword, ...restBody} = req.body
+
+  if(restBody.password === repassword){
+    const user = new userModel(restBody)
+    await user.save()
+    res.redirect('/auth')
+  } else {
+    res.redirect('/register?err=repassword')
+  }
+})
+
+app.get('/auth', (req, res) => {
+  const {error} = req.query
+  res.render('auth', {error})
+})
+
+app.post('/auth', passport.authenticate)
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/auth')
+})
 
 start()
