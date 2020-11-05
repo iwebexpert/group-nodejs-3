77a8537e7e51f1ed6f49b0973d06deac5e09bc56
rusla <suhrugen@gmail.com>
2020-11-05 19:48:22 +0700

    Набросок hw7

---
+++ sukhorukov/hw7/server/models/user.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const bcryptjs = require('bcryptjs')
+
+const Schema = mongoose.Schema
+
+const SALT_ROUNDS = 10
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    default: 'Безымянный'
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
+userSchema.pre('save', function (next) {
+  if (this.isModified('password')) {
+    const salt = bcryptjs.genSaltSync(SALT_ROUNDS)
+    this.password = bcryptjs.hashSync(this.password, salt)
+  }
+  next()
+})
+
+userSchema.methods.validatePassword = function (password) {
+  return bcryptjs.compareSync(password, this.password)
+}
+
+module.exports = mongoose.model('User', userSchema, 'users')
