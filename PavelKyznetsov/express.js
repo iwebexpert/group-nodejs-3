5a6135756ec10e2ab746f9b1ca8622153ff50b9a
Pavel Kuznetsov <mig38m@gmail.com>
2020-11-05 18:53:57 +0500

    6

--- PavelKyznetsov/express.js
+++ PavelKyznetsov/express.js
@@ -1,20 +1,20 @@
 const express = require('express')
 const hbs = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 const path = require('path')
 const mongoose = require('./config/mongodb')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 const tasksModel = require('./models/tasks')
+const userModel = require('./models/user')
+const passport = require('./auth')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost:27017/tasks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-})
-
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cookieParser())
 app.use(express.static('public'))
 
 app.engine('hbs', hbs({
@@ -24,24 +24,38 @@ app.engine('hbs', hbs({
   partialsDir: path.join(__dirname, 'views', 'partials'),
   helpers: {
     switch: function (value, options) {
-      this.switch_value = value;
-      return options.fn(this);
+      this.switch_value = value
+      return options.fn(this)
     },
     case: function (value, options) {
       if (value === this.switch_value) {
-        return options.fn(this);
+        return options.fn(this)
       }
     },
     ifEquals: function (arg1, arg2, options) {
-      return (arg1 === arg2) ? options.fn(this) : options.inverse(this);
+      return (arg1 === arg2) ? options.fn(this) : options.inverse(this)
     }
   }
 }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  resave: true,
+  saveUninitialized: false,
+  secret: 'secretpass123123123',
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}))
+app.use(passport.initialize)
+app.use(passport.session)
 
-app.get('/', async (req, res) => {
-  const tasksList = await tasksModel.find().lean()
+app.all('/', (req, res) => {
+  res.redirect('/tasks')
+})
+
+app.use(['/task', '/tasks'], passport.mustBeAuthenticated)
+
+app.get('/tasks', async (req, res) => {
+  const tasksList = await tasksModel.where('userId').equals(req.user._id).lean()
   res.render('tasksList', {tasksList})
 })
 
@@ -52,25 +66,66 @@ app.get('/task', async (req, res) => {
 
 app.get('/task/:id', async (req, res) => {
   const task = await tasksModel.findById(req.params.id).lean()
-  res.render('task', {task})
+  if (task.userId === req.user._id)
+    res.render('task', {task})
+  else
+    res.render('error')
 })
 
 app.post('/task', async (req, res) => {
-  const task = new tasksModel(req.body)
-  const taskSaved = await task.save()
-  res.redirect('/')
+  try {
+    const task = new tasksModel(req.body)
+    task.userId = req.user._id
+    const taskSaved = await task.save()
+    res.redirect('/tasks')
+  } catch {
+    res.redirect('/task')
+  }
 })
 
 app.delete('/task/:id', async (req, res) => {
   const task = await tasksModel.findById(req.params.id)
-  await task.deleteOne({_id: req.params.id})
+  if (task.userId === req.user._id)
+    await task.deleteOne({_id: req.params.id})
+  else
+    throw "Error deleting"
 })
 
 app.patch('/task/:id', async (req, res) => {
   const task = await tasksModel.findById(req.params.id)
-  await task.updateOne(req.body)
+  if (task.userId === req.user._id)
+    await task.updateOne(req.body)
+  else
+    throw "Error updating"
 })
 
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+  const {repassword, ...restBody} = req.body
+
+  if(restBody.password === repassword){
+    const user = new userModel(restBody)
+    await user.save()
+    res.redirect('/auth')
+  } else {
+    res.redirect('/register?err=repassword')
+  }
+})
+
+app.get('/auth', (req, res) => {
+  const {error} = req.query
+  res.render('auth', {error})
+})
+
+app.post('/auth', passport.authenticate)
+
+app.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/auth')
+})
 
 app.get('*', (req, res) => {
   res.status(404).render('error')
